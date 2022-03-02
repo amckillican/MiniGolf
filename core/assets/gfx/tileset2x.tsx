8140f5de852e1754733b0fe2353e23c0fc960b1d
mckillican <alexander@systemwright.com>
2022-03-01 20:24:48 -0500

    Added tileset for help mode (will work ok file system)

---
+++ core/assets/gfx/tileset2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset2x" tilewidth="34" tileheight="34" tilecount="2" columns="2">
+ <image source="tileset2x.png" width="68" height="34"/>
+</tileset>
---
+++ core/assets/gfx/tileset4x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset4x" tilewidth="68" tileheight="68" tilecount="2" columns="2">
+ <image source="tileset4x.png" width="136" height="68"/>
+</tileset>
