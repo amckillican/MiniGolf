3358b30e7ec4aadcc673cad752bb92d5be7fdffc
mckillican <alexander@systemwright.com>
2022-03-26 22:39:39 -0400

    Fixed random crashing

--- core/src/com/minigolf/game/minigolf.java
+++ core/src/com/minigolf/game/minigolf.java
@@ -98,92 +98,94 @@ public class Minigolf extends Game implements InputProcessor {
 		// keep track of the game time in seconds
 		currentFrame += Gdx.graphics.getDeltaTime();
 
-		// scrolling the background infinitely
-		bgPos1 += 1;
-		bgPos2 += 1;
+		if (gamestate == "title") {
+			// scrolling the background infinitely
+			bgPos1 += 1;
+			bgPos2 += 1;
 
-		if (bgPos2 >= 1360) {
-			bgPos2 = -1360;
-		} else if (bgPos1 >= 1360) {
-			bgPos1 = -1360;
-		}
-
-		// drawing the sprites
-		batch.begin();
-		batch.draw(bg, bgPos1, 0);
-		batch.draw(bg, bgPos2, 0);
-		batch.end();
-
-		// alpha channel
-		Gdx.gl.glEnable(GL30.GL_BLEND);
-		Gdx.gl.glBlendFunc(GL30.GL_SRC_ALPHA, GL30.GL_ONE_MINUS_SRC_ALPHA);
-
-		// drawing rectangle
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(0, 0, 0, (float) 0.75);
-		shapeRenderer.rect(300, 252, 281, 100);
-		shapeRenderer.rect(300, 62, 281, 100);
-		shapeRenderer.rect(772, 252, 281, 100);
-		shapeRenderer.rect(772, 62, 281, 100);
-		shapeRenderer.end();
-
-		// alpha channel end
-		Gdx.gl.glDisable(GL30.GL_BLEND);
-
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-		if (Gdx.input.getX() >= 300 && Gdx.input.getX() <= 582) {
-			if (Gdx.input.getY() >= 413 && Gdx.input.getY() <= 513) {
-				shapeRenderer.rect(300, 252, 281, 100);
+			if (bgPos2 >= 1360) {
+				bgPos2 = -1360;
+			} else if (bgPos1 >= 1360) {
+				bgPos1 = -1360;
 			}
-		}
 
-		if (Gdx.input.getX() >= 300 && Gdx.input.getX() <= 582) {
-			if (Gdx.input.getY() >= 603 && Gdx.input.getY() <= 703) {
-				shapeRenderer.rect(300, 62, 281, 100);
+			// drawing the sprites
+			batch.begin();
+			batch.draw(bg, bgPos1, 0);
+			batch.draw(bg, bgPos2, 0);
+			batch.end();
 
-				if (Gdx.input.isTouched()) {
-					gamestate = "help";
-					startFrame = currentFrame;
-					this.setScreen(new help(this));
+			// alpha channel
+			Gdx.gl.glEnable(GL30.GL_BLEND);
+			Gdx.gl.glBlendFunc(GL30.GL_SRC_ALPHA, GL30.GL_ONE_MINUS_SRC_ALPHA);
+
+			// drawing rectangle
+			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+			shapeRenderer.setColor(0, 0, 0, (float) 0.75);
+			shapeRenderer.rect(300, 252, 281, 100);
+			shapeRenderer.rect(300, 62, 281, 100);
+			shapeRenderer.rect(772, 252, 281, 100);
+			shapeRenderer.rect(772, 62, 281, 100);
+			shapeRenderer.end();
+
+			// alpha channel end
+			Gdx.gl.glDisable(GL30.GL_BLEND);
+
+			shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+			if (Gdx.input.getX() >= 300 && Gdx.input.getX() <= 582) {
+				if (Gdx.input.getY() >= 413 && Gdx.input.getY() <= 513) {
+					shapeRenderer.rect(300, 252, 281, 100);
 				}
 			}
-		}
 
-		if (Gdx.input.getX() >= 772 && Gdx.input.getX() <= 953) {
-			if (Gdx.input.getY() >= 413 && Gdx.input.getY() <= 513) {
-				shapeRenderer.rect(772, 252, 281, 100);
-			}
-		}
+			if (Gdx.input.getX() >= 300 && Gdx.input.getX() <= 582) {
+				if (Gdx.input.getY() >= 603 && Gdx.input.getY() <= 703) {
+					shapeRenderer.rect(300, 62, 281, 100);
 
-		if (Gdx.input.getX() >= 772 && Gdx.input.getX() <= 953) {
-			if (Gdx.input.getY() >= 603 && Gdx.input.getY() <= 703) {
-				shapeRenderer.rect(772, 62, 281, 100);
-				if (Gdx.input.isTouched()) {
-					Gdx.app.exit();
+					if (Gdx.input.isTouched()) {
+						gamestate = "help";
+						startFrame = currentFrame;
+						this.setScreen(new help(this));
+					}
 				}
 			}
-		}
 
-		shapeRenderer.end();
+			if (Gdx.input.getX() >= 772 && Gdx.input.getX() <= 953) {
+				if (Gdx.input.getY() >= 413 && Gdx.input.getY() <= 513) {
+					shapeRenderer.rect(772, 252, 281, 100);
+				}
+			}
 
-		// display the text on the buttons
-		batch.begin();
-		font.setColor(Color.WHITE);
-		font.getData().setScale(2);
-		font.draw(batch, "NINE HOLE", 325, 327);
-		font.draw(batch, "HELP", 385, 133);
-		font.draw(batch, "ENDLESS", 810, 327);
-		font.draw(batch, "EXIT", 865, 133);
-		font.getData().setScale(1);
-		font.setColor(Color.BLACK);
-		font.draw(batch, "Created By: Adam Fischer, Ben Smith, Alex McKillican, Clinton Osawe", 5, 30);
-		batch.end();
+			if (Gdx.input.getX() >= 772 && Gdx.input.getX() <= 953) {
+				if (Gdx.input.getY() >= 603 && Gdx.input.getY() <= 703) {
+					shapeRenderer.rect(772, 62, 281, 100);
+					if (Gdx.input.isTouched()) {
+						Gdx.app.exit();
+					}
+				}
+			}
 
-		// exiting the application
-		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
-			System.out.println("Close");
-			Gdx.app.exit();
+			shapeRenderer.end();
+
+			// display the text on the buttons
+			batch.begin();
+			font.setColor(Color.WHITE);
+			font.getData().setScale(2);
+			font.draw(batch, "NINE HOLE", 325, 327);
+			font.draw(batch, "HELP", 385, 133);
+			font.draw(batch, "ENDLESS", 810, 327);
+			font.draw(batch, "EXIT", 865, 133);
+			font.getData().setScale(1);
+			font.setColor(Color.BLACK);
+			font.draw(batch, "Created By: Adam Fischer, Ben Smith, Alex McKillican, Clinton Osawe", 5, 30);
+			batch.end();
+
+			// exiting the application
+			if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+				System.out.println("Close");
+				Gdx.app.exit();
+			}
 		}
 
 		if (!gamestate.equals("title")) {
