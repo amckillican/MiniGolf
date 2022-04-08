e7355a2efa5b07fcd56e3d780ac63e109bd9e7cf
SuperMada <adamfishy7@gmail.com>
2022-04-08 09:41:19 -0400

    Added shape rendering, need to figure out drawing it once

--- core/src/com/minigolf/game/Screens/Endless.java
+++ core/src/com/minigolf/game/Screens/Endless.java
@@ -3,10 +3,19 @@ package com.Minigolf.game.Screens;
 import com.Minigolf.game.Minigolf;
 import com.Minigolf.game.Global.Gameplay;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.*;
 
 public class Endless implements Screen {
     public Minigolf game;
-    public static int level = 1;
+    public static int level = 1000;
+    public static ShapeRenderer shapeRenderer;
+    public static ShapeRenderer batch;
+    Texture darkTileIMG = new Texture("gfx/darktile.png");
+    Random rand = new Random();
+
+    
     // create screen
     public Endless(Minigolf game) {
         this.game = game;
@@ -16,7 +25,18 @@ public class Endless implements Screen {
     public void render(float delta) {
         // method for physics, ui, and other things that will be used in other screens
         Gameplay.gameplay();
-
+        // initializing shape renderer
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setAutoShapeType(true);
+        if (Minigolf.currentFrame == Minigolf.startFrame){
+            for(int i = 0; i < Math.ceil(level/2); i++){
+                int x = rand.nextInt(1361);
+                int y = rand.nextInt(766);
+                Minigolf.batch.begin();
+                Minigolf.batch.draw(darkTileIMG, x, y);
+                Minigolf.batch.end();
+            }   
+        }
         
 
         // debugging
