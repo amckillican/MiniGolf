2ebd2920e4cd7d11658b7393a6b2c911b2c37efe
SuperMada <adamfishy7@gmail.com>
2022-04-26 13:32:30 -0400

    This were done, i forgor what

--- core/src/com/minigolf/game/Screens/Endless.java
+++ core/src/com/minigolf/game/Screens/Endless.java
@@ -15,12 +15,11 @@ public class Endless implements Screen {
     public static int change = 1;
     public static ShapeRenderer shapeRenderer;
     public static ShapeRenderer batch;
-    ArrayList<Integer>nums = new ArrayList<>();
+    ArrayList<Integer> nums = new ArrayList<>();
     Texture darkTileIMG = new Texture("gfx/darktile.png");
     Random rand = new Random();
     public static int track = 0;
 
-    
     // create screen
     public Endless(Minigolf game) {
         this.game = game;
@@ -31,45 +30,43 @@ public class Endless implements Screen {
         // method for physics, ui, and other things that will be used in other screens
         Gameplay.gameplay();
         // initializing shape renderer
-		shapeRenderer = new ShapeRenderer();
-		shapeRenderer.setAutoShapeType(true);
-             
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
 
-        //Based on the level, create random coordinates and place obsticals
-        for(int i = 0; i < (int)Math.pow(level*2,1.25); i+=2){
+        // Based on the level, create random coordinates and place obsticals
+        for (int i = 0; i < (int) Math.pow(level * 2, 1.25); i += 2) {
             int x = rand.nextInt(1361);
             int y = rand.nextInt(766);
-            for(int j = 0; j<nums.size(); j+=2){
-                if(x - nums.get(j) > -30 && x - nums.get(j) < 30){
-                    if(y - nums.get(j+1) > -30 && y - nums.get(j+1) < 30){
+            for (int j = 0; j < nums.size(); j += 2) {
+                if (x - nums.get(j) > -30 && x - nums.get(j) < 30) {
+                    if (y - nums.get(j + 1) > -30 && y - nums.get(j + 1) < 30) {
                         track++;
                     }
                 }
             }
-            //if(track==0){
+            if (track == 0) {
                 nums.add(x);
                 nums.add(y);
-            //}
-            //else{
-            //    track = 0;
-            //}
+            } else
+                track = 0;
             Minigolf.batch.begin();
-            Minigolf.batch.draw(darkTileIMG, nums.get(i), nums.get(i+1));
+            Minigolf.batch.draw(darkTileIMG, nums.get(i), nums.get(i + 1));
             Minigolf.batch.end();
-        } 
-        if(change!=level){
+        }
+        if (change != level) {
+            // Collections.shuffle(nums);
             nums.clear();
             change = level;
         }
-        //if(level>?) - put in water trap
-        //if(level>?) - put in sand trap
-        //if(level>?) - put in trap trap
-        if(Gdx.input.isKeyPressed(Keys.SPACE)){
+        // if(level>?) - put in water trap
+        // if(level>?) - put in sand trap
+        // if(level>?) - put in trap trap
+        if (Gdx.input.isKeyPressed(Keys.SPACE)) {
             level++;
             System.out.println(level);
         }
-        if(Gdx.input.isKeyPressed(Keys.P)){
-            level=1;
+        if (Gdx.input.isKeyPressed(Keys.P)) {
+            level = 1;
             System.out.println(level);
         }
     }
