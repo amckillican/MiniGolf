33c505dd7cab69fcfa564b263f3ca29858610a82
SuperMada <adamfishy7@gmail.com>
2022-05-06 13:24:03 -0400

    priority

--- core/src/com/minigolf/game/Screens/Endless.java
+++ core/src/com/minigolf/game/Screens/Endless.java
@@ -32,7 +32,6 @@ public class Endless implements Screen {
     public static int sandx = 0;
     public static int sandy = 0;
 
-
     // create screen
     public Endless(Minigolf game) {
         this.game = game;
@@ -44,17 +43,42 @@ public class Endless implements Screen {
         Minigolf.batch.begin();
         Minigolf.batch.draw(BenFace, 0, 0);
         Minigolf.batch.end();
+
         // method for physics, ui, and other things that will be used in other screens
         Gameplay.gameplay();
+
         // initializing shape renderer
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setAutoShapeType(true);
+
+        Minigolf.batch.begin();
+
+        // Add sand pit co-ordinates
+        if (start) {
+            sandx = rand.nextInt(400);
+            sandy = rand.nextInt(766);
+            sand = rand.nextInt(3) + 1;
+            start = false;
+        }
+
+        // Randomly choses a sandpit sprite and places it
+        if (sand == 1) {
+            Minigolf.batch.draw(sand1, sandx, sandy);
+        }
+        if (sand == 2) {
+            Minigolf.batch.draw(sand2, sandx, sandy);
+        }
+        if (sand == 3) {
+            Minigolf.batch.draw(sand3, sandx, sandy);
+        }
 
         // Based on the level, create random coordinates
         for (int i = 0; i < (int) Math.pow(level * 2, 1.25); i += 2) {
             int x = rand.nextInt(1361);
             int y = rand.nextInt(766);
-            // Check if level is completable, distance between blocks is larger than ball size
+
+            // Check if level is completable
+            // distance between blocks is larger than ball size
             for (int j = 0; j < nums.size(); j += 2) {
                 if (x - nums.get(j) > -30 && x - nums.get(j) < 30) {
                     if (y - nums.get(j + 1) > -30 && y - nums.get(j + 1) < 30) {
@@ -62,46 +86,29 @@ public class Endless implements Screen {
                     }
                 }
             }
-            // Add co-ordinates to array
+
+            // Add coordinates to array
             if (track == 0) {
                 nums.add(x);
                 nums.add(y);
             } else {
                 track = 0;
             }
-            Minigolf.batch.begin();
-            // Add sand pit co-ordinates
-            if (start) {
-                sandx = rand.nextInt(400);
-                sandy = rand.nextInt(766);
-                sand = rand.nextInt(3) + 1;
-                start = false;
-            }
-            // Randomly choses a sandpit sprite and places it
-            if(sand==1){
-                Minigolf.batch.draw(sand1, sandx, sandy);
-            }
-            if(sand==2){
-                Minigolf.batch.draw(sand2, sandx, sandy);
-            }
-            if(sand==3){
-                Minigolf.batch.draw(sand3, sandx, sandy);
-            }
+
             // Attempts to place block obstacle
             try {
                 Minigolf.batch.draw(darkTileIMG, nums.get(i), nums.get(i + 1));
                 // If there is no index in the array game doesn't crash
-            } catch (Exception e) {
-            }
-            Minigolf.batch.end();
+            } catch (Exception e) {}
         }
+
         // If the level changes clear the array to place new blocks
         if (change != level) {
-            // Collections.shuffle(nums);
             nums.clear();
             change = level;
             start = true;
         }
+
         // if(level>?) - put in water trap
         if (Gdx.input.isKeyPressed(Keys.SPACE)) {
             level++;
@@ -111,8 +118,8 @@ public class Endless implements Screen {
             level = 1;
             System.out.println(level);
         }
+
         // Show current level
-        Minigolf.batch.begin();
         Minigolf.font.setColor(1, 1, 1, 1);
         Minigolf.font.getData().setScale(1);
         Minigolf.font.draw(Minigolf.batch, "Level: " + level, 10, 720);
