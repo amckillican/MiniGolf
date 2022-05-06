b5f0335474283c6afe280563a3cbebd049e8493f
SuperMada <adamfishy7@gmail.com>
2022-05-06 12:49:41 -0400

    Commented code, added bens face and resized sand

--- core/src/com/minigolf/game/Screens/Endless.java
+++ core/src/com/minigolf/game/Screens/Endless.java
@@ -24,6 +24,7 @@ public class Endless implements Screen {
     Texture sand1 = new Texture("gfx/Sand1.png");
     Texture sand2 = new Texture("gfx/Sand2.png");
     Texture sand3 = new Texture("gfx/Sand3.png");
+    Texture BenFace = new Texture("gfx/background.jpg");
     Random rand = new Random();
     public static int track = 0;
     public static BitmapFont font;
@@ -39,16 +40,21 @@ public class Endless implements Screen {
 
     @Override
     public void render(float delta) {
+        // Set backround
+        Minigolf.batch.begin();
+        Minigolf.batch.draw(Minigolf.bg, 0, 0);
+        Minigolf.batch.end();
         // method for physics, ui, and other things that will be used in other screens
         Gameplay.gameplay();
         // initializing shape renderer
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setAutoShapeType(true);
 
-        // Based on the level, create random coordinates and place obsticals
+        // Based on the level, create random coordinates
         for (int i = 0; i < (int) Math.pow(level * 2, 1.25); i += 2) {
             int x = rand.nextInt(1361);
             int y = rand.nextInt(766);
+            // Check if level is completable, distance between blocks is larger than ball size
             for (int j = 0; j < nums.size(); j += 2) {
                 if (x - nums.get(j) > -30 && x - nums.get(j) < 30) {
                     if (y - nums.get(j + 1) > -30 && y - nums.get(j + 1) < 30) {
@@ -56,6 +62,7 @@ public class Endless implements Screen {
                     }
                 }
             }
+            // Add co-ordinates to array
             if (track == 0) {
                 nums.add(x);
                 nums.add(y);
@@ -63,12 +70,14 @@ public class Endless implements Screen {
                 track = 0;
             }
             Minigolf.batch.begin();
+            // Add sand pit co-ordinates
             if (start) {
                 sandx = rand.nextInt(400);
                 sandy = rand.nextInt(766);
                 sand = rand.nextInt(3) + 1;
                 start = false;
             }
+            // Randomly choses a sandpit sprite and places it
             if(sand==1){
                 Minigolf.batch.draw(sand1, sandx, sandy);
             }
@@ -78,12 +87,15 @@ public class Endless implements Screen {
             if(sand==3){
                 Minigolf.batch.draw(sand3, sandx, sandy);
             }
+            // Attempts to place block obstacle
             try {
                 Minigolf.batch.draw(darkTileIMG, nums.get(i), nums.get(i + 1));
+                // If there is no index in the array game doesn't crash
             } catch (Exception e) {
             }
             Minigolf.batch.end();
         }
+        // If the level changes clear the array to place new blocks
         if (change != level) {
             // Collections.shuffle(nums);
             nums.clear();
@@ -91,8 +103,6 @@ public class Endless implements Screen {
             start = true;
         }
         // if(level>?) - put in water trap
-        // if(level>?) - put in sand trap
-        // if(level>?) - put in trap trap
         if (Gdx.input.isKeyPressed(Keys.SPACE)) {
             level++;
             System.out.println(level);
@@ -101,6 +111,7 @@ public class Endless implements Screen {
             level = 1;
             System.out.println(level);
         }
+        // Show current level
         Minigolf.batch.begin();
         Minigolf.font.setColor(1, 1, 1, 1);
         Minigolf.font.getData().setScale(1);
