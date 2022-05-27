78e56a13ede314825d467ad705e5dc7b098f8ff6
bsmith4972 <82818955+bsmith4972@users.noreply.github.com>
2022-05-27 12:22:11 -0400

    ff

--- desktop/src/com/Minigolf/game/desktop/DesktopLauncher.java
+++ desktop/src/com/Minigolf/game/desktop/DesktopLauncher.java
@@ -7,8 +7,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-	
-		
+
 		// config settings
 		config.setWindowedMode(1360, 765);
 		config.setTitle("MiniGolf");
